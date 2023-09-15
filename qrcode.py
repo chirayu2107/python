ec5cc6395051c67444bf6aca635edfb5fae084b7
chirayu2107 <144110269+chirayu2107@users.noreply.github.com>
2023-09-15 10:25:52 +0530

    qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -3,22 +3,17 @@ import qrcode
 
 data = "https://www.primevideo.com/"
 
-# Generate a QR code
 qr = qrcode.QRCode(
-    version=1,  # QR code version (adjust as needed)
-    error_correction=qrcode.constants.ERROR_CORRECT_L,  # Error correction level
-    box_size=10,  # Size of each QR code "box" (adjust as needed)
-    border=4,     # Border size (adjust as needed)
+    version=1,  
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,  
+    border=4,     # Border size
 )
 
 qr.add_data(data)
 qr.make(fit=True)
 
-# Create an image of the QR code
 img = qr.make_image(fill_color="black", back_color="white")
 
-# Save the image or display it
 img.save("qrcode.png")  # Save to a file
-img.show()  # Display in default image viewer
-
-
+img.show()
